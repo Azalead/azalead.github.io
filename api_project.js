d86714edc10a4aebe22c689c12bfb0ac09346b88
azalead-jenkins <developer@azalead.com>
2018-05-28 16:27:47 +0000

    Publish "azalead-core-api" "documentation" version "0.28.11"

--- api_project.js
+++ api_project.js
@@ -87,7 +87,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-05-23T13:01:45.034Z",
+    "time": "2018-05-25T09:31:31.322Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
