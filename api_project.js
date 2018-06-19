cc384975ccac378b7a9bbc595be3701df47fd864
azalead-jenkins <developer@azalead.com>
2018-06-19 15:40:36 +0000

    Publish "azalead-core-api" "documentation" version "0.29.2"

--- api_project.js
+++ api_project.js
@@ -1,6 +1,6 @@
 define({
   "name": "REST API",
-  "version": "0.28.0",
+  "version": "0.29.1",
   "description": "Jabmo REST API documentation",
   "title": "Jabmo REST API",
   "url": "https://api.jabmo.com",
@@ -87,7 +87,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-06-18T08:21:50.705Z",
+    "time": "2018-06-19T08:57:56.979Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
