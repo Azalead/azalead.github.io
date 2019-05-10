844e282e1e477ea5e29cd5f52dc3aae3d760f5bc
azalead-jenkins <developer@azalead.com>
2019-05-10 18:42:13 +0000

    Publish "azalead-cid-api" "documentation" version "3.5.86"

--- cid/api_project.js
+++ cid/api_project.js
@@ -20,7 +20,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-04-30T09:07:10.463Z",
+    "time": "2019-05-10T18:31:26.288Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
